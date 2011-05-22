3b9bb21179e6ddd8e1b2fb491abc237626255810
Jean-Philippe Fleury <contact@jpfleury.net>
2011-05-22 15:33:47 -0400

    - Utilisation de webkit à la place de gtkhtml2, qui est déprécié. - Utilisation de gettext pour pouvoir traduire le greffon. - Traduction en français de `markdownpreview.gedit-plugin`. - Modification du raccourci `Ctrl+Maj+M` pour `Ctrl+Alt+M` (raccourci pour afficher l'aperçu). - Affichage du panneau inférieur (s'il ne l'est pas déjà) lors de l'activation du greffon. - Affichage de l'onglet *Aperçu Markdown* (s'il ne l'est pas déjà) lors de la demande d'affichage de l'aperçu. - Formatage du code.
    Merci à ZedTux, Frédéric Bertolus et Matthew Cantelon pour leurs suggestions et propositions de patchs.

--- plugins/markdownpreview.py
+++ plugins/markdownpreview.py
@@ -5,7 +5,7 @@
 # HTML preview of Markdown formatted text in gedit
 # Auteur: Michele Campeotto
 # Copyright © Michele Campeotto, 2005, 2006.
-# Copyright © Jean-Philippe Fleury, 2009. <contact@jpfleury.net>
+# Copyright © Jean-Philippe Fleury, 2009, 2011. <contact@jpfleury.net>
 
 # Ce programme est un logiciel libre; vous pouvez le redistribuer ou le
 # modifier suivant les termes de la GNU General Public License telle que
@@ -22,102 +22,100 @@
 # <http://www.gnu.org/licenses/>.
 
 import gedit
-
 import sys
 import gtk
-import gtkhtml2
+import webkit
 import markdown
+import gettext
+from gpdefs import *
 
-HTML_TEMPLATE = """<html><head><meta http-equiv="content-type"
-content="text/html; charset=UTF-8" /><style type="text/css">
-body { background-color: #fff; padding: 8px; }
-p, div { margin: 0em; }
-p + p, p + div, div + p, div + div { margin-top: 0.8em; }
-blockquote { padding-left: 12px; padding-right: 12px; }
-pre { padding: 12px; }
-</style></head><body>%s</body></html>"""
+try:
+	APP_NAME = 'markdown-preview'
+	LOCALE_PATH = os.path.dirname(__file__) + '/markdown-preview-locale'
+	gettext.bindtextdomain(APP_NAME, GP_LOCALEDIR)
+	_ = lambda s: gettext.dgettext(APP_NAME, s);
+except:
+	_ = lambda s: s
+
+HTML_TEMPLATE = """<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<style type="text/css">
+			body {background-color: #fff; padding: 8px;}
+			p, div {margin: 0em;}
+			p + p, p + div, div + p, div + div {margin-top: 0.8em;}
+			blockquote {padding-left: 12px; padding-right: 12px;}
+			pre {padding: 12px;}
+		</style>
+	</head>
+	<body>%s</body>
+</html>"""
 
 class MarkdownPreviewPlugin(gedit.Plugin):
-
 	def __init__(self):
 		gedit.Plugin.__init__(self)
-			
+	
 	def activate(self, window):
-		action = ("Markdown Preview",
-			  None,
-			  "Markdown Preview",
-			  "<Control><Shift>M",
-			  "Update the HTML preview",
-			  lambda x, y: self.update_preview(y))
+		action = ("Markdown Preview", None, _("Markdown Preview"), "<Control><Alt>M",
+		          _("Update the HTML preview"), lambda x, y: self.update_preview(y))
 		
-		# Store data in the window object
+		# Store data in the window object.
 		windowdata = dict()
 		window.set_data("MarkdownPreviewData", windowdata)
 	
 		scrolled_window = gtk.ScrolledWindow()
-		scrolled_window.set_property("hscrollbar-policy",gtk.POLICY_AUTOMATIC)
-		scrolled_window.set_property("vscrollbar-policy",gtk.POLICY_AUTOMATIC)
-		scrolled_window.set_property("shadow-type",gtk.SHADOW_IN)
-
-		html_view = gtkhtml2.View()
-		html_doc = gtkhtml2.Document()
-		html_view.set_document(html_doc)
+		scrolled_window.set_property("hscrollbar-policy", gtk.POLICY_AUTOMATIC)
+		scrolled_window.set_property("vscrollbar-policy", gtk.POLICY_AUTOMATIC)
+		scrolled_window.set_property("shadow-type", gtk.SHADOW_IN)
 		
-		html_doc.clear()
-		html_doc.open_stream("text/html")
-		html_doc.write_stream(HTML_TEMPLATE % ("",))
-		html_doc.close_stream()
-
-		scrolled_window.set_hadjustment(html_view.get_hadjustment())
-		scrolled_window.set_vadjustment(html_view.get_vadjustment())
+		html_view = webkit.WebView()
+		html_view.load_string((HTML_TEMPLATE % ("", )), "text/html", "utf-8", "file:///")
+		
 		scrolled_window.add(html_view)
 		scrolled_window.show_all()
 		
-		bottom = window.get_bottom_panel()
+		bottom_panel = window.get_bottom_panel()
 		image = gtk.Image()
 		image.set_from_icon_name("gnome-mime-text-html", gtk.ICON_SIZE_MENU)
-		bottom.add_item(scrolled_window, "Markdown Preview", image)
-		windowdata["bottom_panel"] = scrolled_window
-		windowdata["html_doc"] = html_doc
+		bottom_panel.add_item(scrolled_window, _("Markdown Preview"), image)
+		bottom_panel.show()
 		
+		windowdata["bottom_panel"] = scrolled_window
+		windowdata["html_doc"] = html_view
 		windowdata["action_group"] = gtk.ActionGroup("MarkdownPreviewActions")
-		windowdata["action_group"].add_actions ([action], window)
+		windowdata["action_group"].add_actions([action], window)
 
 		manager = window.get_ui_manager()
 		manager.insert_action_group(windowdata["action_group"], -1)
 
-		windowdata["ui_id"] = manager.new_merge_id ()
+		windowdata["ui_id"] = manager.new_merge_id()
 
-		manager.add_ui (windowdata["ui_id"],
-				"/MenuBar/ToolsMenu/ToolsOps_5",
-				"Markdown Preview",
-				"Markdown Preview",
-				gtk.UI_MANAGER_MENUITEM, 
-				True)
+		manager.add_ui(windowdata["ui_id"], "/MenuBar/ToolsMenu/ToolsOps_5",
+		               "Markdown Preview", "Markdown Preview", gtk.UI_MANAGER_MENUITEM, True)
 	
 	def deactivate(self, window):
-		# Retreive the data of the window object
+		# Retreive data of the window object.
 		windowdata = window.get_data("MarkdownPreviewData")
 		
-		# Remove the menu action
+		# Remove the menu action.
 		manager = window.get_ui_manager()
 		manager.remove_ui(windowdata["ui_id"])
 		manager.remove_action_group(windowdata["action_group"])
 		
-		# Remove the bottom panel
-		bottom = window.get_bottom_panel()
-		bottom.remove_item(windowdata["bottom_panel"])
+		# Remove Markdown Preview from the bottom panel.
+		bottom_panel = window.get_bottom_panel()
+		bottom_panel.remove_item(windowdata["bottom_panel"])
 	
 	def update_preview(self, window):
-		# Retreive the data of the window object
+		# Retreive data of the window object.
 		windowdata = window.get_data("MarkdownPreviewData")
 		
 		view = window.get_active_view()
+		
 		if not view:
-			 return
+			return
 		
 		doc = view.get_buffer()
-		
 		start = doc.get_start_iter()
 		end = doc.get_end_iter()
 		
@@ -126,14 +124,13 @@ class MarkdownPreviewPlugin(gedit.Plugin):
 			end = doc.get_iter_at_mark(doc.get_selection_bound())
 		
 		text = doc.get_text(start, end)
-		html = HTML_TEMPLATE % (markdown.markdown(text),)
-		
+		html = HTML_TEMPLATE % (markdown.markdown(text), )
 		p = windowdata["bottom_panel"].get_placement()
-		
 		html_doc = windowdata["html_doc"]
-		html_doc.clear()
-		html_doc.open_stream("text/html")
-		html_doc.write_stream(html)
-		html_doc.close_stream()
-		
+		html_doc.load_string(html, "text/html", "utf-8", "file:///")
 		windowdata["bottom_panel"].set_placement(p)
+		
+		bottom_panel = window.get_bottom_panel()
+		bottom_panel.show()
+		bottom_panel.activate_item(windowdata["bottom_panel"])
+
