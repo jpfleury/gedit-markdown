bb4fd96e6e36303d3bd04022b18480eb5983c858
Jean-Philippe Fleury <687082+jpfleury@users.noreply.github.com>
2025-05-19 15:34:42 -0400

    Fix #21 (install paths)

--- gedit-markdown.sh
+++ gedit-markdown.sh
@@ -18,36 +18,57 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ################################################################################
+## @title Setup
+################################################################################
+
+set -euo pipefail
+trap 'echo "Error on line $LINENO: $BASH_COMMAND" >&2' ERR
+trap 'exit 1' INT TERM
+
+################################################################################
 ## @title Functions
 ################################################################################
 
-# Note that if a directory doesn't have read permissions, the function can't
-# test if it contains files.
-is_empty() {
-	if [[ -d $1 && -r $1 ]]; then
+is_empty_dir() {
+	local dir=$1
+	
+	# --------------------
+	
+	# Return false if:
+	# - not a directory, or
+	# - not readable (we can't test its contents), or
+	# - is a symlink
+	if [[ ! -d $dir || ! -r $dir || -L $dir ]]; then
+		return 1
+	fi
+
+	# Subshell to avoid changing the global shopt state
+	if (
 		shopt -s nullglob dotglob
 		declare -a files
-		files=("$1"/*)
-		shopt -u nullglob dotglob
-
-		if [[ ${#files[@]} == 0 ]]; then
-			return 0
-		fi
+		files=("$dir"/*)
+		(( ${#files[@]} == 0 ))
+	); then
+		return 0
 	fi
 
 	return 1
 }
 
 remove_empty_dirs() {
-	local dir
+	local dir parent
 	
 	# --------------------
 	
 	for dir in "$@"; do
-		while is_empty "$dir"; do
-			rmdir -v "$dir"
+		while is_empty_dir "$dir"; do
+			rmdir -v "$dir" || break
 			
-			dir=$(dirname "$dir")
+			parent=$(dirname -- "$dir")
+			
+			[[ $parent == "$dir" || $parent == / ]] && break
+			
+			dir=$parent
 		done
 	done
 }
@@ -56,40 +77,66 @@ remove_plugin() {
 	local file
 	declare -a files_to_remove
 	declare -a empty_dirs_to_remove
-	
+
 	# --------------------
-	
-	# Remove files
-	##############
-	
+
+	# Files
+	#######
+
 	files_to_remove=(
-		"$PATH_PLUGIN/markdown-preview.gedit-plugin"
-		"$PATH_SNIPPETS/markdown.xml"
-		"$PATH_TOOLS/export-to-html"
-		"$PATH_TOOLS/export-to-pdf"
+		"$PATH_SNIPPETS_GEDIT/markdown.xml"
+		"$PATH_TOOLS_GEDIT/export-to-html"
+		"$PATH_TOOLS_GEDIT/export-to-pdf"
+		"$PATH_PLUGINS_GEDIT/markdown-preview.plugin"
+		
+		# Obsolete
+		"$PATH_CONFIG_GEDIT/gedit-markdown.ini"
+		"$PATH_PLUGINS_GEDIT/markdown-preview.gedit-plugin"
 	)
-	
+
 	for file in "${files_to_remove[@]}"; do
-		if [[ -f $file ]]; then
-			rm -v "$file"
+		if [[ ! -e $file ]]; then
+			echo "File not found: $file"
+			
+			continue
 		fi
+		
+		if [[ ! -f $file || -L $file ]]; then
+			echo "Ignoring file: $file"
+			
+			continue
+		fi
+		
+		rm -v "$file"
 	done
 
-	# Remove directories
-	####################
-	
+	# Directories
+	#############
+
 	empty_dirs_to_remove=(
 		"$PATH_CONFIG"
-		"$PATH_PLUGIN"
-		"$PATH_MARKDOWN_PREVIEW_PLUGIN"
-		"$PATH_SNIPPETS"
-		"$PATH_TOOLS"
+		"$PATH_PLUGINS_GEDIT"
+		"$PATH_PLUGIN_MARKDOWN_PREVIEW"
+		"$PATH_SNIPPETS_GEDIT"
+		"$PATH_TOOLS_GEDIT"
 	)
 	
-	if [[ -d $PATH_MARKDOWN_PREVIEW_PLUGIN ]]; then
-		rm -rv "$PATH_MARKDOWN_PREVIEW_PLUGIN"
+	if [[ ! -e $PATH_CONFIG ]]; then
+		echo "Directory not found: $PATH_CONFIG"
+	elif [[ ! -d $PATH_CONFIG || -L $PATH_CONFIG ]]; then
+		echo "Ignoring directory: $PATH_CONFIG"
+	else
+		rm -rv "$PATH_CONFIG"
 	fi
 	
+	if [[ ! -e $PATH_PLUGIN_MARKDOWN_PREVIEW ]]; then
+		echo "Directory not found: $PATH_PLUGIN_MARKDOWN_PREVIEW"
+	elif [[ ! -d $PATH_PLUGIN_MARKDOWN_PREVIEW || -L $PATH_PLUGIN_MARKDOWN_PREVIEW ]]; then
+		echo "Ignoring directory: $PATH_PLUGIN_MARKDOWN_PREVIEW"
+	else
+		rm -rv "$PATH_PLUGIN_MARKDOWN_PREVIEW"
+	fi
+
 	remove_empty_dirs "${empty_dirs_to_remove[@]}"
 }
 
@@ -97,17 +144,33 @@ remove_plugin() {
 ## @title Constants
 ################################################################################
 
-PATH_PLUGIN=${XDG_DATA_HOME:-$HOME/.local/share}/gedit/plugins
-PATH_MARKDOWN_PREVIEW_PLUGIN=$PATH_PLUGIN/markdown-preview
+# gedit
+#######
 
-PATH_CONFIG_GEDIT=${XDG_CONFIG_HOME:-$HOME/.config}
+PATH_CONFIG_GEDIT=${XDG_CONFIG_HOME:-$HOME/.config}/gedit
+PATH_SNIPPETS_GEDIT=$PATH_CONFIG_GEDIT/snippets
+PATH_TOOLS_GEDIT=$PATH_CONFIG_GEDIT/tools
+
+PATH_DATA_GEDIT=${XDG_DATA_HOME:-$HOME/.local/share}/gedit
+PATH_PLUGINS_GEDIT=$PATH_DATA_GEDIT/plugins
+
+declare -r PATH_CONFIG_GEDIT PATH_SNIPPETS_GEDIT PATH_TOOLS_GEDIT
+declare -r PATH_DATA_GEDIT PATH_PLUGINS_GEDIT
+
+# gedit-markdown
+################
 
 PATH_CONFIG=$PATH_CONFIG_GEDIT/markdown-preview
-PATH_SNIPPETS=$PATH_CONFIG_GEDIT/snippets
-PATH_TOOLS=$PATH_CONFIG_GEDIT/tools
 
-declare -r PATH_PLUGIN PATH_MARKDOWN_PREVIEW_PLUGIN PATH_CONFIG_GEDIT
-declare -r PATH_CONFIG PATH_SNIPPETS PATH_TOOLS
+PATH_PLUGIN_MARKDOWN_PREVIEW=$PATH_PLUGINS_GEDIT/markdown-preview
+
+declare -r PATH_CONFIG PATH_PLUGIN_MARKDOWN_PREVIEW
+
+################################################################################
+## @title Arguments
+################################################################################
+
+action=${1:-}
 
 ################################################################################
 ## @title Script
@@ -115,50 +178,80 @@ declare -r PATH_CONFIG PATH_SNIPPETS PATH_TOOLS
 
 cd "$(dirname "$0")" || { echo >&2 "Can't access $0"; exit 1; }
 
-if [[ $1 == install ]]; then
+if [[ $action == install ]]; then
 	echo "# gedit-markdown install"
 	echo "########################"
-	echo ""
-	
-	# Just in case it's an update (not first install)
-	remove_plugin
+	echo
 
+	# In case this is an update
+	###########################
+	
+	echo "Removing previous installation (if any)..."
+	remove_plugin
+	echo
+	
 	# Configuration
+	###############
+	
+	echo "Proceeding with installation..."
 	mkdir -pv "$PATH_CONFIG"
-	cp -rnv config/* "$PATH_CONFIG"
+	cp -rv --update=none config/* "$PATH_CONFIG"
 
 	# Code snippets
-	mkdir -pv "$PATH_SNIPPETS"
-	cp -v snippets/markdown.xml "$PATH_SNIPPETS"
+	###############
+
+	mkdir -pv "$PATH_SNIPPETS_GEDIT"
+	cp -v snippets/markdown.xml "$PATH_SNIPPETS_GEDIT"
 
 	# External tools
-	mkdir -pv "$PATH_TOOLS"
-	cp -v tools/export-to-html "$PATH_TOOLS"
-	chmod +x "$PATH_TOOLS/export-to-html"
-	cp -v tools/export-to-pdf "$PATH_TOOLS"
-	chmod +x "$PATH_TOOLS/export-to-pdf"
+	################
+
+	mkdir -pv "$PATH_TOOLS_GEDIT"
+
+	cp -v tools/export-to-html "$PATH_TOOLS_GEDIT"
+	chmod +x "$PATH_TOOLS_GEDIT/export-to-html"
+
+	cp -v tools/export-to-pdf "$PATH_TOOLS_GEDIT"
+	chmod +x "$PATH_TOOLS_GEDIT/export-to-pdf"
 
 	# Markdown Preview plugin
-	mkdir -pv "$PATH_PLUGIN"
-	cp -rv plugins/markdown-preview/* "$PATH_PLUGIN"
-	rm -v "$PATH_MARKDOWN_PREVIEW_PLUGIN/locale/markdown-preview.pot"
-	find "$PATH_MARKDOWN_PREVIEW_PLUGIN/locale/" -name "*.po" -exec rm -v {} \;
+	#########################
 
+	mkdir -pv "$PATH_PLUGINS_GEDIT"
+	
+	if [[ ! -L $PATH_PLUGINS_GEDIT/markdown-preview.plugin ]]; then
+		cp -v plugins/markdown-preview/markdown-preview.plugin "$PATH_PLUGINS_GEDIT"
+	fi
+	
+	mkdir -pv "$PATH_PLUGIN_MARKDOWN_PREVIEW"
+	
+	if [[ ! -L $PATH_PLUGIN_MARKDOWN_PREVIEW ]]; then
+		cp -rv plugins/markdown-preview/markdown-preview/* "$PATH_PLUGIN_MARKDOWN_PREVIEW"
+		rm -v "$PATH_PLUGIN_MARKDOWN_PREVIEW/locale/markdown-preview.pot"
+		find "$PATH_PLUGIN_MARKDOWN_PREVIEW/locale/" -name "*.po" -exec rm -v {} \;
+	fi
+
+	echo
 	echo "Installation successful. Please restart gedit (if it's already running)."
+	echo
 
 	exit 0
-elif [[ $1 == uninstall ]]; then
+elif [[ $action == uninstall ]]; then
 	echo "# gedit-markdown uninstall"
 	echo "##########################"
-	echo ""
-	
+	echo
+
 	remove_plugin
-	
+
+	echo
 	echo "Uninstallation successful. Please restart gedit (if it's already running)."
+	echo
 
 	exit 0
 else
+	echo
 	echo "Usage: $0 [install|uninstall]"
+	echo
 
 	exit 1
 fi
